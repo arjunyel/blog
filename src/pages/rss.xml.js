98382561206a7f69bfe5b9c6b1959d0098b4d762
arjunyel <arjunyel@gmail.com>
2023-10-10 13:09:18 -0500

    configure rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function get(context) {
   const posts = await getCollection("blog");
@@ -10,7 +13,9 @@ export async function get(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
+      content: sanitizeHtml(parser.render(post.body)),
       link: `/blog/${post.slug}/`,
     })),
+    stylesheet: "/rss/styles.xsl",
   });
 }
